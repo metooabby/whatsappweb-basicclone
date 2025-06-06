4b90e46ab33f9c1a7bbdf732227a49f0e557a75e
Anurag <anuragbhelsewale21@gmail.com>
2025-06-06 18:29:37 +0530

    main.js changs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Import Bootstrap CSS
+
 import 'bootstrap/dist/css/bootstrap.min.css'
-// Optional: Bootstrap JS (for modals, dropdowns, etc.)
 import 'bootstrap'
 
 createApp(App).mount('#app')
